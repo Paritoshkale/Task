6b26f76f9d1ef8c1d16af2d80072b103ae470f32
Paritoshkale <kale.paritosh4062@gmail.com>
2024-12-29 12:56:56 +0530

    Create task8.js
    Use a for...of loop

---
+++ task8.js
@@ -0,0 +1,7 @@
+//   Use a for...of loop to iterate over an array of fruits:
+        //   const fruits = ["apple", "banana", "cherry"]; Log each fruit to the console.
+
+        const fruits = ["apple", "banana", "cherry"];
+        for (let val of fruits) {
+            console.log(val);
+        }
